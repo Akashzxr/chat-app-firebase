4cc7de1873302bd8d03195db1ecf2f5e7e34c5a0
Akash ks <akashks8855@gmail.com>
2022-11-07 23:03:37 +0530

    added search bar to sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,10 +1,12 @@
 import Userbar from "./userbar/userbar";
+import Chatsearch from "./chatsearch/chatsearch";
 import "./sidebar.css";
 
 export default function Sidebar(){
     return(
        <div className="sidebar" >
            <Userbar/>
+           <Chatsearch/>
        </div>
     );
 }
---
+++ src/components/sidebar/chatsearch/chatsearch.js
@@ -0,0 +1,13 @@
+import "./chatsearch.css";
+
+export default function Chatsearch(){
+    return(
+        <div className="chatsearch">
+
+            <input
+            type={"text"} 
+            placeholder="Search chat" />
+
+        </div>
+    )
+}
