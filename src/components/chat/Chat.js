14e14cb2c9f4834020a64b0eea0c37e419ca17d8
Akash ks <akashks8855@gmail.com>
2022-11-09 21:50:29 +0530

    created m

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,10 +1,16 @@
 import "./chat.css";
 import Header from "./header/header";
+import Chatsection from "./chat-section/chat-section";
+import Footer from "./footer/footer";
 
 export default function Chat(){
     return(
         <div className="chat">
-           <Header/>
+            <div className="container">
+               <Header/>
+              <Chatsection/>
+              <Footer/>
+            </div>
         </div>
     );
 }
---
+++ src/components/chat/footer/footer.js
@@ -0,0 +1,11 @@
+import "./footer.css"
+
+export default function Footer(){
+    return(
+        <div className="footer">
+            <div>imoji</div>
+            <input type={"text"} placeholder="Aa"/>
+            <div>sent</div>
+        </div>
+    )
+}
