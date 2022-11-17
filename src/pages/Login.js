395ac9d22a4c79d158352c0a0a374b1ff9873528
Akash ks <akashks8855@gmail.com>
2022-11-17 12:02:06 +0530

    redirecting to app after login success

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,18 +3,19 @@ import { NavLink , useNavigate} from 'react-router-dom'
 import { FaEnvelope } from "react-icons/fa"
 import "./login.css"
 import { useSelector, useDispatch } from 'react-redux'
-import { login } from '../redux/Authslice'
+import { login,signinuser } from '../redux/Authslice'
 
 
 function Login() {
-   const user = useSelector((state)=>state.user);
+   const {user,isSignin} = useSelector((state)=>state.auth);
    const dispatch = useDispatch();
 
     const navigate = useNavigate();
 
     useEffect(()=>{
-        if(user){
+        if(isSignin){
            navigate('/app');
+           console.log(use)
         }
     },[user])
 
@@ -22,7 +23,7 @@ function Login() {
     <div className='login-section'>
         <div className='signin'>
             <FaEnvelope className='signin-logo'/>
-            <button onClick={()=>{dispatch(login())}} className='signin-btn'>sign in with Google</button>
+            <button onClick={()=>{dispatch(signinuser())}} className='signin-btn'>sign in with Google</button>
         </div>
     </div>
 
--- src/redux/Authslice.js
+++ src/redux/Authslice.js
@@ -1,9 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { redirect, useNavigate } from "react-router-dom";
 import googleLogin from "../services/googleLogin";
+
 
 const initialState = {
     user: null,
+    isSignin:false,
 }
+
+//user signin
+export const signinuser = createAsyncThunk('createuser',async () => {
+    try{
+        return await googleLogin()
+    }catch(error){
+        console.log("error")
+    }
+})
+
 
 export const Authslice = createSlice({
     name: "auth",
@@ -11,7 +25,22 @@ export const Authslice = createSlice({
     reducers:{
         login: async (state) =>{
             state.user = await  googleLogin()
+            
         }
+    },
+    extraReducers: (builder) => {
+        builder
+        .addCase(signinuser.pending, (state)=>{
+            state.isSignin= false;
+        })
+        .addCase(signinuser.fulfilled, (state,action)=>{
+            state.isSignin=true;
+            state.user = action.payload
+        })
+        .addCase(signinuser.rejected, (state,action)=>{
+            state.isSignin=false;
+            console.log("rejected")
+        })
     }
 });
 
