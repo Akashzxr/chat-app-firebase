9420dd560ec97d6fbd65ae125c0937c23c0f644f
Akash ks <akashks8855@gmail.com>
2022-12-10 16:07:52 +0530

    created user search function in handlekeydown and displaying user in user container success

--- src/components/sidebar/usersearch/UserSearch.js
+++ src/components/sidebar/usersearch/UserSearch.js
@@ -1,16 +1,56 @@
 import "./UserSearch.css";
 import { useDispatch } from 'react-redux';
 import { canceluserbox } from "../../../redux/Dataslice";
+import { useEffect, useState } from "react";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import {db} from "../../../services/firebase"
 
 function UserSearch() {
+    const [username,setusername]=useState();
+    const [user,setuser] = useState();
     const dispatch = useDispatch();
+
+    const handleKeydown=async(e)=>{
+      if(e.key=='Enter'){
+        // Create a reference to the users collection
+        const usersRef = collection(db, "users");
+        // Create a query against the collection.
+        const q = query(usersRef, where("displayName", "==", username));
+
+        try{
+          const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+              console.log(doc.data())
+              setuser(doc.data());
+            });
+         }catch(error){
+           if(error){
+             console.log("error");
+           }
+         }
+     }
+    }
+
+    useEffect(()=>{
+
+    },[user,username])
+
 
   return (
     <div className="UserSearch-container">
 
         <div className="UserSearch">
-            <input type={"text"} placeholder="search"/>
-            <div className="users-container"></div>
+            <input type={"text"} placeholder="search" onKeyDown={handleKeydown} onChange={(e)=>{setusername(e.target.value)}}/>
+            <div className="users-container">
+
+              {user ? 
+                <div className="search-result">
+                    <img className="result-user-img" src={user.photoURL}/>
+                    <span>{user.displayName}</span>
+                </div> 
+              : null}
+
+            </div>
             <button className="cancel-btn" onClick={()=>{dispatch(canceluserbox())}}>
                 Cancel
             </button>
